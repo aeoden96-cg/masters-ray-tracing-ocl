fb08ebc606fe0908aa72029c19ce00b3f4353252
mateo <mateo1559@gmail.com>
2022-10-07 22:05:20 +0200

    init

---
+++ Sphere.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <CL/opencl.hpp>
+
+class Sphere {
+  public:
+	cl_float radius;
+	cl_float dummy1;
+	cl_float dummy2;
+	cl_float dummy3;
+	cl_float3 position;
+	cl_float3 color;
+	cl_float3 emission;
+};
